3b509fe61bf4977901714a98e38cd7c33e40c02d
AleshaFozu <lexalis12@mail.ru>
2025-05-15 22:02:54 +0700

    ss

--- new-react-site/src/pages/Devlog.tsx
+++ new-react-site/src/pages/Devlog.tsx
@@ -4,13 +4,13 @@ import '../styles/Devlog.css';
 
 const Devlog: React.FC = () => {
   const devlogEntries = [
-    { date: '15.05', content: 'Настроенно CI CD для сайта' },
     { date: '4.04', content: 'Написанно первое qt приложение' },
     { date: '26.01', content: 'Переделан сайт для всех устройств' },
     { date: '26.01', content: 'Доделаны страницы С++' },
     { date: '19.01', content: 'Полный ре-дизайн сайта' },
     { date: '12.01', content: 'Начало разработки новой версии сайта на React' },
-    { date: '12.04', content: 'Переписан сайт на React' }
+    { date: '12.04', content: 'Переписан сайт на React' },
+    { date: '15.05', content: 'Настроенно CI CD для сайта' }
   ].reverse();
 
   return (
